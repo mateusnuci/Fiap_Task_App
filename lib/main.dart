36f72a5982dd44110fbd24cdf856eaae137de271
Mateus Nuci <mateusnucicosta@gmail.com>
2024-10-08 11:47:27 -0300

    supabase api linked and multi provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_app/pages/home/home_page.dart';
+import 'package:todo_app/provider/task_group_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://xwvkgplsgtymhcxzmnwc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3dmtncGxzZ3R5bWhjeHptbndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzOTU2MDgsImV4cCI6MjA0Mzk3MTYwOH0.2DVTYteQzqHBzmoqbUV9UOZbF44n3uE6C5K2Qe_CcW0',
   );
 
-  runApp(const MyApp());
+  runApp (
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TaskGroupProvider()..listTaskGroups())
+      ],
+      child: const MyApp(),
+      ));
 }
 
 class MyApp extends StatelessWidget {
